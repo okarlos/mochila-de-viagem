9dba257965179164b3afd99498cc84101226415b
Karlos Kliemann <karloskliemann@gmail.com>
2022-10-17 07:10:33 -0300

    update js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,50 @@
-const form = document.getElementById("novoItem")
-const ulLista = document.getElementById("lista")
+const form = document.getElementById("novoItem");
+const ulList = document.getElementById("lista");
+const backpack = JSON.parse(localStorage.getItem("backpack")) || [];
+
+backpack.forEach( (element) => {
+    createElement(element);
+})
+
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const item = event.target.elements["nome"].value;
-    const quantidade = event.target.elements["quantidade"].value;
-    createElement(item, quantidade);
+
+    const name = event.target.elements["nome"].value;
+    const quantity = event.target.elements["quantidade"].value;
+    
+    const exist = backpack.find( element => element.name === nome.value );
+
+    const thisItem = {
+        "name": name,
+        "quantity": quantity
+    }
+
+    if (exist) {
+        thisItem.id = exist.id;
+        updateElement(thisItem);
+    }else {
+        thisItem.id = backpack.length;
+        createElement(thisItem);
+        backpack.push(thisItem);
+    }
+
+    localStorage.setItem("backpack", JSON.stringify(backpack));
+    form.reset();
 })
 
-function createElement(item, quantidade) {
-    console.log(item);
-    console.log(quantidade);
-
+function createElement(item) {
     const strong = document.createElement("strong");
-    strong.innerHTML = quantidade;
+    strong.innerHTML = item.quantity;
+    strong.dataset.id = item.id;
 
     const li = document.createElement("li");
     li.appendChild(strong);
     li.classList.add("item");
-    li.innerHTML += item ;
+    li.innerHTML += item.name ;
+    ulList.appendChild(li);
+}
 
-    ulLista.appendChild(li);
+function updateElement(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantity;
 }
